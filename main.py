e04bb55e6c98b7ecc41cae97fa3152adef3ed5e9
AyuAyu <107897218+AyukiFukami@users.noreply.github.com>
2024-06-16 16:53:06 +0900

    一時的に日付をいつにもできるような形に変更できるような形に変更

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
-from os import environ
+import os
+import json
 from datetime import datetime
 from edinet import Edinet
 
+# ユーザーから日付を入力させる
+date_str = input("Please enter the date in YYYY-MM-DD format: ")
+
+# 日付を解析し、datetimeオブジェクトに変換する
+try:
+    date = datetime.strptime(date_str, "%Y-%m-%d")
+except ValueError:
+    print("Invalid date format. Please use YYYY-MM-DD format.")
+    exit(1)
 
 # APIキーを環境変数"API_KEY"から持ってくる
-API_KEY = environ["API_KEY"]
+API_KEY = os.environ["API_KEY"]
 
 edi = Edinet(API_KEY)
 
-#ドキュメント一覧を取得
-document_list = edi.get_document_list(datetime.today(), type_=2)
+# ドキュメント一覧を取得
+document_list = edi.get_document_list(date, type_=2)
 
-#seccodeをキーとした辞書の作成
-documents_class_by_seccode: dict[str, list[tuple[str, str, str]]] = {}
+# seccodeをキーとした辞書の作成
+# key = SecCode, value = (docDescription, docID, filerName)
+documents_class_by_secCode: dict[str, list[tuple[str, str, str]]] = {}
+
+for document in document_list["results"]:
+    # legalStatusが0の場合は閲覧期間満了
+    if document["legalStatus"] != 0:
+        # 辞書の要素に今まで出たことがないSecCodeだった場合、key, valueを作成
+        if not documents_class_by_secCode.get(document["secCode"]):
+            documents_class_by_secCode[document["secCode"]] = []
+        documents_class_by_secCode[document["secCode"]].append((
+            document["docDescription"],
+            document["docID"],
+            document["filerName"]
+        ))
+
+# 保存する
+with open("documents.json", "w", encoding="utf-8") as f:
+    f.write(json.dumps(documents_class_by_secCode, indent=4, ensure_ascii=False))
